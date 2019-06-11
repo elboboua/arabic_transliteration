4a72aec75b7478efb3d78613cd09f0d8b161451c
elboboua <44094673+elboboua@users.noreply.github.com>
2019-06-11 12:50:03 +0300

    Added a few functions to process the english string
    These functions handle vowels, tanwin, and al at-ta3reef.

--- main.js
+++ main.js
@@ -53,12 +53,13 @@ var vowel_dict = {
   "آ": "ā",
 
   // short vowels
+  // the tanwin are marked with ñ so they can be detected in a later function
   "\u064E": "a",
   "\u0650": "i",
   "\u064F": "u",
-  "\u064B": "an",
-  "\u064D": "in",
-  "\u064C": "un",
+  "\u064B": "añ",
+  "\u064D": "iñ",
+  "\u064C": "uñ",
   "\u0652": "",
   "\u0651": "$" // shadda is shown as a $ sign
 }
@@ -76,6 +77,102 @@ var arabic_chars_dict = Object.assign({},
   vowel_dict,
   punctuation_dict)
 
+// this function isolates the al at-ta3reef with a hyphen
+// the problem is that we aren't picking up on the tanwin
+// we need to change the tanwin to another symbol
+function definite_article_separator(str) {
+  new_string = str
+
+  for (i = 0; i < str.length; i++) {
+
+    if (i == 0 && str[i] == "a") {
+        try {
+          if (str[i+1] == "l") {
+            new_string = new_string.substr(0,i+2) + "-" + new_string.substr(i+2);
+          }
+        } catch {
+          console.log("Something has gone wrong")
+        }
+    }
+    else if (i != 0 && i != str.length-1) {
+      if (str[i - 1] == " " && str[i + 1] == "l") {
+        new_string = new_string.substr(0,i+2) + "-" + new_string.substr(i+2);
+        //console.log("a hyphen was added.")
+      }
+    }
+  }
+    return new_string;
+
+}
+
+// This function removes final alif after accusative tanwin alif
+// and makes tanwin ñ into n
+function normalize_tanwin(str) {
+  for (i = 0; i < str.length-1; i++) {
+    if (str[i] == "ñ" && str[i+1] != " ") {
+      str = str.substr(0,i) + "n" + str.substr(i+2);
+    } else if (str[i] == "ñ") {
+      str = str.substr(0,i) + "n" + str.substr(i+1)
+    }
+    }
+    return str;
+}
+
+// this function changes vowels to glides in initial and final syllable positions
+function vowels_to_glides(str) {
+  for (i = 0; i < str.length; i++) {
+    // intial position of the document
+    if (i == 0) {
+      switch(str[i]) {
+        case "u":
+          str = "w" + str.substr(i+1);
+          break;
+        case "i":
+          str = "y" + str.substr(i+1);
+          break;
+        default:
+          break;
+      }
+    } // if "i" is initial position before another vowel
+    else if (str[i] == "i" && (str[i+1] == "a" || str[i+1] == "u")) {
+      str = str.substr(0,i)+ "y" + str.substr(i+1);
+    }// if "u" is initial position before another vowel
+    else if (str[i] == "u" && (str[i+1] == "a" || str[i+1] == "i")) {
+      str = str.substr(0,i)+ "w" + str.substr(i+1);
+    } // this is for syllable final "i"
+    else if (str[i] == "i" && str[i-1] == "a") {
+      str = str.substr(0,i)+ "y" + str.substr(i+1);
+    } // this is for syllable final "u"
+    else if (str[i] == "u" && str[i-1] == "a") {
+      str = str.substr(0,i)+ "w" + str.substr(i+1);
+    }
+  }
+
+  return str;
+}
+
+
+// This function creates long vowels by finding two side by side vowels.
+// A function that changes i&u to y&w in initial positions and after "a"
+// should come before
+// unfortunately there is no control for unmarked medial hamzatul wasl positions
+// and it will print as a long "a"
+function create_long_vowels(str) {
+  for (i = 0; i < str.length-1; i++) {
+    if (str[i] == "a" && str[i+1] == "a") {
+      str = str.substr(0,i) + "ā" + str.substr(i+2);
+    } else if (str[i] == "u" && str[i+1] == "u") {
+      str = str.substr(0,i) + "ū" + str.substr(i+2);
+    } else if (str[i] == "i" && str[i+1] == "i") {
+      str = str.substr(0,i) + "ī" + str.substr(i+2);
+    }
+  }
+
+  return str;
+}
+
+
+// this function takes the raw arabic script and converts to roman script from the dictionary
 function convert(str) {
   new_string = ""
 
@@ -91,9 +188,17 @@ function convert(str) {
 
 function transcribe() {
   arabicText = document.getElementById("arabicText").value;
-  englishText = convert(arabicText)
+  output = convert(arabicText);
+  // there needs to be a function that looks for regular words that start with "al" and replaces them
+  output = definite_article_separator(output);
+  // shadda function here
+  output = normalize_tanwin(output);
+  output = vowels_to_glides(output);
+  output = create_long_vowels(output);
+  // hamzatul wasl function here
 
-  document.getElementById("englishText").value = englishText;
+
+  document.getElementById("englishText").value = output;
 }
 
 document.getElementById("button").addEventListener("click", transcribe)
